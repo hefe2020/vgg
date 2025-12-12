4078d4487e5a97a4d0ef5afc791d1c95d3fc048e
Jesse Gilbert <75207775+hefe2020@users.noreply.github.com>
2025-12-12 07:56:16 +0000

    Update infer.py

--- src/vgg/infer.py
+++ src/vgg/infer.py
@@ -7,7 +7,7 @@ from .model import TinyVGGSample
 def load_model(model_path, class_to_idx_path):
   class_idx = json.load(open(class_to_idx_path))
   num_class = len(class_idx)
-  model = TinyVGGSample(input=3, hidden=20, output=num_class)
+  model = TinyVGGSample(input=3, hidden=40, output=num_class)
   model.load_state_dict(torch.load(model_path, map_location='cpu'))
   model.eval()
   return model, class_idx
